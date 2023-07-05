380692cb176cec6ec5a32f5627c404093c29ce77
Jose Bernardo Velasquez Alzate <theskatalites@gmail.com>
2023-07-04 20:31:28 -0500

    Simple demo Using Parallel Streams, generating a list of random numbers and filtering odd or even sending them in Parallel.

--- src/main/java/edu/dev/demo/Main.java
+++ src/main/java/edu/dev/demo/Main.java
@@ -1,5 +1,79 @@
-package edu.dev.demo;public class Main {
+package edu.dev.demo;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+@Slf4j
+public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        var numbers = getListOfIntegers();
+
+        var start = System.currentTimeMillis();
+        log.info("Current Thread: {}", Thread.currentThread().getName());
+
+        numbers.parallelStream()
+                .filter(number -> number % 2 == 0)
+                .forEach(Main::evenNumbers);
+
+        numbers.parallelStream()
+                .filter(number -> number % 2 != 0)
+                .forEach(Main::oddNumbers);
+
+        var end = System.currentTimeMillis();
+        log.info("Current Thread: {}, final time: {}", Thread.currentThread().getName(), end - start);
     }
+
+    private static ArrayList<Integer> getListOfIntegers() {
+        var maxValue = 100;
+        var minValue = 1;
+        var size = 10;
+        var random = new Random();
+        var numbers = new ArrayList<Integer>();
+
+        for (int i = 0; i < size; i++) {
+            int randomNumber = random.nextInt(maxValue - minValue + 1) + minValue;
+            numbers.add(randomNumber);
+        }
+
+        return numbers;
+    }
+
+    private static void oddNumbers(Integer oddNumber) {
+        try {
+
+            var start = System.currentTimeMillis();
+            log.info("Current Thread: {}", Thread.currentThread().getName());
+
+            log.info("This is a odd numbers: {}", oddNumber);
+            Thread.sleep(50);
+
+            var end = System.currentTimeMillis();
+            log.info("Current Thread: {}, final time: {}", Thread.currentThread().getName(), end - start);
+
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+        }
+    }
+
+    private static void evenNumbers(Integer evenNumber) {
+        try {
+
+            var start = System.currentTimeMillis();
+            log.info("Current Thread: {}", Thread.currentThread().getName());
+
+            log.info("This is a even numbers: {}", evenNumber);
+            Thread.sleep(50);
+
+            var end = System.currentTimeMillis();
+            log.info("Current Thread: {}, final time: {}", Thread.currentThread().getName(), end - start);
+
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+        }
+    }
+
 }
